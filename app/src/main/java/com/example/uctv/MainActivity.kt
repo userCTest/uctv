15a6c2e90c9525ebc67547bb98f608a9e14dbdcb
Rui Reis <rui.reis@usercentrics.com>
2023-02-24 15:15:11 +0000

    upgrade to 2.7.13

--- app/src/main/java/com/example/uctv/MainActivity.kt
+++ app/src/main/java/com/example/uctv/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.uctv
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentActivity
+import com.usercentrics.sdk.GeneralStyleSettings
 import com.usercentrics.sdk.Usercentrics
 import com.usercentrics.sdk.UsercentricsOptions
 import com.usercentrics.sdk.ui.tv.BannerSettings
@@ -12,6 +14,7 @@ import com.usercentrics.sdk.ui.tv.UsercentricsBanner
  */
 class MainActivity : FragmentActivity() {
 
+    val LOG_TAG = "[USERCENTRICS][UCTV]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,15 +22,22 @@ class MainActivity : FragmentActivity() {
         val options = UsercentricsOptions(settingsId = "egLMgjg9j")
         Usercentrics.initialize(this, options)
 
-        Usercentrics.isReady({
-            val banner = UsercentricsBanner(
-                context = this,
-                settings = BannerSettings()
-            )
-            banner.showFirstLayer { userResponse ->
-                // Handle userResponse
-                print(userResponse)
+        Usercentrics.isReady({ status ->
+            Log.d(LOG_TAG, "shouldCollectConsent - ${status.shouldCollectConsent}")
+            if (status.shouldCollectConsent) {
+                // Show banner to collect consent
+                val banner = UsercentricsBanner(
+                    context = this,
+                    settings = BannerSettings()
+                )
+                banner.showFirstLayer { userResponse ->
+                    // Handle userResponse
+                    print(userResponse)
+                }
+            } else {
+                // Apply consent with status.consents
             }
+
         }, {
             print("UC not ready")
         })
